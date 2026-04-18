82ad92e8d3c1dbcfeae336223bcc488644100454
osamamahmouddev <osama.mahmoud.moahmaed@gmail.com>
2026-04-18 12:20:32 +0200

    - just fake push

--- lib/core/networking/api_constant.dart
+++ lib/core/networking/api_constant.dart
@@ -31,6 +31,7 @@ class ApiConstant {
   static const String addCustomer = 'Customer';
   static const String customerplan = 'customerPlan';
   static const String branches = 'Branch';
+  
   static const String accountRequest = 'AccountRequest';
   static const String plan = 'Plan';
   static const String Task = 'api/Task';
