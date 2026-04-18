3d67bc8a5533f5db0b225bc9bfa6141a9ac55c73
saher alaa <151808466+saheralaa@users.noreply.github.com>
2026-04-18 17:27:08 +0200

    update: nject payslip resources into the application

--- lib/core/dependecy_injection/service_locator.dart
+++ lib/core/dependecy_injection/service_locator.dart
@@ -2,28 +2,31 @@ import 'package:get_it/get_it.dart';
 import 'package:hr_management_system_package/core/notifications/notification_repo.dart';
 import 'package:hr_management_system_package/core/notifications/notifications_repo_impl.dart';
 import 'package:hr_management_system_package/core/repos/shared_repo.dart';
-import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_attendance_repo/supervisor_attendance_repo_impl.dart';
+import 'package:hr_management_system_package/employee_infrastructure/data/repo/employee_payslip_repo/employee_payslip_repo.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/employee_summary_repo/employee_summary_repo_impl.dart';
+import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_attendance_repo/supervisor_attendance_repo_impl.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_leave_requests_repo/supervisor_leave_requests_repo_impl.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_news_repo/supervisor_news_repo_impl.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_plans_repo/supervisor_plan_repo_impl.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 import '../../admin_infrastructure/data/repo/shifts_and_polices_repo/shifts_and _polices_repo_impl.dart';
 import '../../admin_infrastructure/data/repo/shifts_and_polices_repo/shifts_and_polices_repo.dart';
 import '../../employee_infrastructure/data/repo/employee_attendance_repo/employee_attendance_repo.dart';
 import '../../employee_infrastructure/data/repo/employee_attendance_repo/employee_attendance_repo_impl.dart';
+import '../../employee_infrastructure/data/repo/employee_payslip_repo/employee_payslip_repo_impl.dart';
 import '../../hr_manamgement_system_package.dart';
 import '../../register_account/repo/register_account_repo.dart';
 import '../../register_account/repo/register_account_repo_impl.dart';
-import '../../supervisor_infrastructure/data/repo/supervisor_attendance_repo/supervisor_attendance_repo.dart';
 import '../../supervisor_infrastructure/data/repo/employee_summary_repo/employee_summary_repo.dart';
+import '../../supervisor_infrastructure/data/repo/supervisor_attendance_repo/supervisor_attendance_repo.dart';
 import '../../supervisor_infrastructure/data/repo/supervisor_leave_requests_repo/supervisor_leave_requests_repo.dart';
 import '../../supervisor_infrastructure/data/repo/supervisor_news_repo/supervisor_news_repo.dart';
 import '../../supervisor_infrastructure/data/repo/supervisor_plans_repo/supervisor_plan_repo.dart';
 import '../../supervisor_infrastructure/data/repo/supervisor_tasks_repo/supervisor_tasks_repo.dart';
 import '../../supervisor_infrastructure/data/repo/supervisor_tasks_repo/supervisor_tasks_repo_impl.dart';
-import '../common_methods/network_checker.dart';
 import '../common_methods/check_accessiable_area_service.dart';
+import '../common_methods/network_checker.dart';
 import '../repos/shared_repo_impl.dart';
 
 final getIt = GetIt.instance;
@@ -141,6 +144,11 @@ void setUpServiceLocator() {
 
   getIt.registerSingleton<AdminManageEmployeeRepo>(
     AdminManageEmployeeRepoImpl(
+      apiService: getIt<ApiService>(),
+    ),
+  );
+  getIt.registerSingleton<EmployeePayslipRepo>(
+    EmployeePayslipRepoImpl(
       apiService: getIt<ApiService>(),
     ),
   );
