58cf34dd6a9cd3e6d0eaae856ab20658e07d4527
osamamahmouddev <osama.mahmoud.moahmaed@gmail.com>
2026-07-29 15:17:29 +0300

    chore: update environment configurations to use new demo base URL

--- lib/env/env.g.dart
+++ lib/env/env.g.dart
@@ -10,12 +10,12 @@ part of 'env.dart';
 // ignore_for_file: type=lint
 // generated_from: .env.dev
 final class _EnvDev {
-  static const String baseUrl = 'http://emsdemo.runasp.net/api/';
+  static const String baseUrl = 'http://emstothrms.runasp.net/api/';
 }
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
 // generated_from: .env.prod
 final class _EnvProd {
-  static const String baseUrl = 'http://emsdemo.runasp.net/api/';
+  static const String baseUrl = 'http://emstothrms.runasp.net/api/';
 }
